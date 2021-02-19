c3b39ced572287f1ce3bdaa9aefda9e72dba61cf
devdw98 <devdw98@gmail.com>
2021-02-19 17:55:21 +0900

    base

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,46 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/xxx">xxx</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <site-title :title="mytitle" />
+      <!-- <v-toolbar-title>WelcomeToTheWorld</v-toolbar-title> -->
+      <v-spacer />
+      <v-btn icon to="/about">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-navigation-drawer app v-model="drawer">
+      <site-menu />
+    </v-navigation-drawer>
+    <v-content>
+      <router-view />
+    </v-content>
+    <!-- <v-main>
+      <HelloWorld />
+    </v-main> -->
+    <site-footer :footer="myfooter" />
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+// import HelloWorld from './components/HelloWorld'
+import SiteTitle from '@/views/layout/title'
+import SiteFooter from '@/views/layout/footer'
+import SiteMenu from '@/views/layout/menu'
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {
+    SiteTitle,
+    SiteFooter,
+    SiteMenu
+  },
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+  data: () => ({
+    drawer: false,
+    mytitle: 'WelcomeToTheWorld',
+    myfooter: 'myfootertest'
+  })
 }
-</style>
+</script>
---
+++ src/views/layout/footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-footer app absolute>
+    <v-spacer />
+    <div>
+      &copy;
+      {{ new Date().getFullYear() + ' ' + footer }}
+    </div>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  props: ['footer']
+}
+</script>
+
+<style lang="scss" scoped></style>
