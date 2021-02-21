07c7fa49e8a9512bdddbc732c64b5a41a5ae69ed
dw <devdw98@gmail.com>
2021-02-21 18:02:04 +0900

    menu

--- src/views/layout/menu.vue
+++ src/views/layout/menu.vue
@@ -2,27 +2,56 @@
   <div>
     <v-list-item>
       <v-list-item-content>
-        <v-list-item-title class="title"> Application </v-list-item-title>
-        <v-list-item-subtitle> subtext </v-list-item-subtitle>
+        <v-list-item-title class="title">WelcomeToTheWorld</v-list-item-title>
+        <v-list-item-subtitle> 관리자페이지 </v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
     <v-divider></v-divider>
     <v-list dense nav>
-      <v-list-item v-for="item in items" :key="item.title" link>
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
+      <v-list-group
+        v-for="game in games"
+        :key="game.title"
+        :value="game.isOpen"
+        :prepend-icon="game.icon"
+      >
+        <template v-slot:activator>
+          <v-list-item-title>{{ game.title }}</v-list-item-title>
+        </template>
 
-        <v-list-item-content>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+        <v-list-group
+          v-for="(item, i) in game.menus"
+          :key="i"
+          no-action
+          sub-group
+        >
+          <template v-slot:activator>
+            <v-list-item-title>
+              {{ item.title }}
+            </v-list-item-title>
+          </template>
+          <v-list-item v-for="(subItem, i) in item.sub" :key="i" link>
+            <v-list-item-title>{{ subItem }}</v-list-item-title>
+          </v-list-item>
+        </v-list-group>
+      </v-list-group>
     </v-list>
   </div>
 </template>
 <script>
 export default {
   data: () => ({
+    games: [
+      {
+        title: '냐한남자',
+        icon: 'mdi-view-dashboard',
+        isOpen: true,
+        menus: [
+          { title: '회원' },
+          { title: '우편함', sub: ['우편 생성', '우편 목록'] }
+        ]
+      },
+      { title: '게임 미정', icon: 'mdi-help-box', isOpen: false }
+    ],
     items: [
       { title: 'Dashboard', icon: 'mdi-view-dashboard' },
       { title: 'Photos', icon: 'mdi-image' },
